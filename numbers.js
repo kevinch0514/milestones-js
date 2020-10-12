f49834c600ecd10f9555d31f79a5378694c5d9a1
Kevin Chen <kevinch0514@gmail.com>
2020-10-12 17:40:09 -0600

    preliminary outline and work on numbers

---
+++ numbers.js
@@ -0,0 +1,71 @@
+console.log('Numbers');
+
+// ASSIGN TWO VARIABLES
+
+let num1 = 123;
+let num2 = 982;
+
+console.log(`The value of num1 is: ${num1}`);
+console.log(`The value of num2 is: ${num2}`);
+
+
+// ADDITION
+let sum = num1 + num2;
+console.log(`The sum of num1 + num2 is: ${sum}`);
+
+
+// SUBTRACTION
+let difference = num1 - num2;
+console.log(`The difference between num1 - num2 is: ${difference}`);
+
+
+// MULTIPLICATION
+let product = num1 * num2;
+console.log(`The product of num1 * num2 is: ${product}`);
+
+
+// DIVISION
+let quotient = num1 / num2;
+console.log(`The quotient of num1 / num2 is: ${quotient}`);
+
+// REMAINDER
+let remainder = num1 % num2;
+console.log(`The remainder of num1 % num2 is: ${remainder}`);
+
+// POWER
+let power = num1 ** num2;
+console.log(`The power of num1 ** num2 is: ${power}`);
+
+// FLOOR
+let floor = Math.floor(num1 / num2);
+console.log(`The floor of Math.floor(num1 / num2) is: ${floor}`);
+
+
+console.log('Manipulating Variables');
+
+let blah = 4562;
+console.log(`The value of blah is: ${blah}`);
+
+// RIGHT MOST DIGIT
+let rightMostDigit = blah % 10;
+console.log(`The right most digit of ${blah} is: ${rightMostDigit}`);
+
+// EVEN or ODD
+let evenOrOdd = blah % 2
+
+// Increment by 1
+blah += 1;
+console.log(`The value of blah incremented by 1 is now: ${blah}`);
+
+// Increment by 2
+blah += 2;
+console.log(`The value of blah incremented by 2 is now: ${blah}`);
+
+// Increment by 10
+blah += 10;
+console.log(`The value of blah incremented by 10 is now: ${blah}`);
+
+// Increment by k
+let k = 12
+blah += k;
+console.log(`The value of blah incremented by k, ${k}, is now: ${blah}`);
